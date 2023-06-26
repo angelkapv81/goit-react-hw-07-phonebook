1b12faa7bdb5d7c7d94fe3bff1c12996020e4465
angelkapv81 <angelkapv81@gmail.com>
2023-06-26 23:07:47 +0300

    add form

--- src/components/Pages/Login .jsx
+++ src/components/Pages/Login .jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>сторінка НЕ для реєстрації</div>;
+  return (
+    <div>
+      сторінка НЕ для реєстрації
+      <form
+      // onSubmit={handleSubmit}
+      >
+        <label>
+          Email
+          <input
+            type="email"
+            // value={name} onChange={handleNameChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password
+          <input
+            type="text"
+            // value={email}
+            // onChange={handleEmailChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Ввійти</button>
+      </form>
+    </div>
+  );
 };
 export default Login;
--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,6 +1,50 @@
 import React from 'react';
 
 const Register = () => {
-  return <div>сторінка для реєстрації</div>;
+  return (
+    <div>
+      сторінка для реєстрації
+      <form
+      // onSubmit={handleSubmit}
+      >
+        <label>
+          Ім'я:
+          <input
+            type="text"
+            // value={name} onChange={handleNameChange}
+          />
+        </label>
+        <br />
+        <label>
+          Електронна пошта:
+          <input
+            type="email"
+            // value={email}
+            // onChange={handleEmailChange}
+          />
+        </label>
+        <br />
+        <label>
+          Пароль:
+          <input
+            type="text"
+            // value={email}
+            // onChange={handleEmailChange}
+          />
+        </label>
+        <br />
+        <label>
+          Повторити пароль:
+          <input
+            type="text"
+            // value={email}
+            // onChange={handleEmailChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Зареєструватися</button>
+      </form>
+    </div>
+  );
 };
 export default Register;
